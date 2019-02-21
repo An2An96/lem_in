a7445668f56f279b8a1b1a194ff4f49ad6e6fbb6
Watty Ballabar <wballaba@e1r11p2.21-school.ru>
2019-02-21 17:25:24 +0300

    find best comb paths

--- srcs/let_ants_to_path.c
+++ srcs/let_ants_to_path.c
@@ -3,16 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   let_ants_to_path.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rschuppe <rschuppe@student.42.fr>          +#+  +:+       +#+        */
+/*   By: wballaba <wballaba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/20 19:25:43 by rschuppe          #+#    #+#             */
-/*   Updated: 2019/02/20 19:27:09 by rschuppe         ###   ########.fr       */
+/*   Updated: 2019/02/21 17:23:05 by wballaba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-void	let_ants_to_path(int **path, t_node *rooms)
+void	let_ants_to_path(t_path **path, t_node *rooms, t_farm *farm)
 {
+	int	finished_ants;
 
+	finished_ants = 0;
+	while (finished_ants <= farm->ants_count)
+	{
+		
+	}
+	
 }
